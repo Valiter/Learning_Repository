056723260306059458b0f15a5f69781e754956ff
antonkuzmichev <an2000n@icloud.com>
2021-02-13 13:18:14 +0300

    [upd] [13.02.21] [13:18]

---
+++ Learning/lesson_2_task_6.py
@@ -0,0 +1,11 @@
+"""ЗАДАНИЕ СО ЗВЕЗДОЧКОЙ!!!"""
+# {key_1:"", hey_2:"", key_3:""}
+# ( num , {})
+#
+
+print("Составление структуры данных 'Товары'.")
+things_list = []
+number = input("Введите номер товара: ")
+things_list.append(number)
+print(things_list.index(number))
+# Зачем кортежи?
