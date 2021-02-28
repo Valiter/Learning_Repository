973694edf9c43ecc6c1718233400b09c53e0168d
antonkuzmichev <an2000n@icloud.com>
2021-02-28 13:58:09 +0300

    [upd] [28.02.21] [13:57] [comment: Задание 3 не выходит.]

--- Learning/lesson_5/lesson_5_task_3.py
+++ Learning/lesson_5/lesson_5_task_3.py
@@ -1,15 +1,24 @@
 
 def counter_for_company(cm = "company_file.txt"):
     st = True
+    money = 0
+    money_list = []
     file = open(cm)
     list = []
+    length = 0
+    n = 0
     while st == True:
         line = file.readline()
         ls = line.split(" - ")
         list.append(ls)
+        length += 1
         if line == "":
             st = False
     file.close
-    print(list)
+    while n < length:
+        print(list[n][1])
+        n += 1
+    return money_list
 
-counter_for_company()
+res = counter_for_company()
+print(res)
