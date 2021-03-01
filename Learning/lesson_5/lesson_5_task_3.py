e0722ad693ee02f7501ea42ea405d3adcae75278
antonkuzmichev <an2000n@icloud.com>
2021-03-01 21:38:47 +0300

    [upd] [01.03.21] [21:38] [comment: ]

--- Learning/lesson_5/lesson_5_task_3.py
+++ Learning/lesson_5/lesson_5_task_3.py
@@ -1,24 +1,50 @@
+"""
+3. Создать текстовый файл (не программно), построчно записать фамилии сотрудников и
+величину их окладов. Определить,
+кто из сотрудников имеет оклад менее 20 тыс., вывести фамилии этих сотрудников.
+Выполнить подсчет средней величины дохода сотрудников.
+"""
+
 
 def counter_for_company(cm = "company_file.txt"):
     st = True
-    money = 0
+    money = []
     money_list = []
     file = open(cm)
     list = []
     length = 0
     n = 0
-    while st == True:
-        line = file.readline()
-        ls = line.split(" - ")
-        list.append(ls)
-        length += 1
-        if line == "":
-            st = False
-    file.close
-    while n < length:
-        print(list[n][1])
-        n += 1
+    try:
+        while st == True:
+            line = file.readline()
+            ls = line.split(" - ")
+            list.append(ls)
+            length += 1
+            if line == "":
+                st = False
+        file.close
+        while n < length:
+            print(list[n][1])
+            money.append(list[n][1])
+            n += 1
+
+    except Exception as err:
+        print(err)
+    print(money)
     return money_list
 
 res = counter_for_company()
 print(res)
+
+
+# incomes = []
+# with open('task_3.txt', 'r') as f_o:
+#     lines = f_o.readlines()
+#
+#     for line in lines:
+#         name, income = line.split(",")
+#         income = int(income)
+#         incomes.append(income)
+#         if income < 20000:
+#             print(name)
+#     print(sum(incomes) / len(incomes))
