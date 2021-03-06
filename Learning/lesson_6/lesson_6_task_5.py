7f1f2a87e4b0ab051ee04fc96f96c331f5b8f811
antonkuzmichev <an2000n@icloud.com>
2021-03-06 12:20:12 +0300

    [upd] [06.03.21] [12:20] [comment: Выполнил пятое задание.]

--- Learning/lesson_6/lesson_6_task_5.py
+++ Learning/lesson_6/lesson_6_task_5.py
@@ -9,3 +9,50 @@
 """
 
 
+class Stationery:
+    def __init__(self, title):
+        self.title = title
+
+    def draw(self):
+        print(f"Запуск отрисовки {self.title}.")
+
+
+class Pen(Stationery):
+    def __init__(self, title="ручка"):
+        self.title = title
+
+    def draw(self):
+        print(f"Запуск отрисовки ручки.")
+
+
+class Pencil(Stationery):
+    def __init__(self, title="карандаш"):
+        self.title = title
+
+    def draw(self):
+        print(f"Запуск отрисовки карандаша.")
+
+
+class Handle(Stationery):
+    def __init__(self, title="маркер"):
+        self.title = title
+
+    def draw(self):
+        print(f"Запуск отрисовки маркера.")
+
+
+name = input("Введите название канцеляского товара: ")
+
+state = Stationery(name)
+pen = Pen()
+pencil = Pencil()
+handle = Handle()
+
+state.draw()
+pen.draw()
+pencil.draw()
+handle.draw()
+
+print(pen.title)
+print(pencil.title)
+print(handle.title)
