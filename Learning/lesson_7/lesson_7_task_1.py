c369e5c9365416b5508c3ad9ca358f7e9a1fb941
antonkuzmichev <an2000n@icloud.com>
2021-03-10 15:15:51 +0300

    [upd] [10.03.21] [14:38] [comment: Скописастил решение, не круто.]

--- Learning/lesson_7/lesson_7_task_1.py
+++ Learning/lesson_7/lesson_7_task_1.py
@@ -12,3 +12,33 @@
 Подсказка: сложение элементов матриц выполнять поэлементно — первый элемент первой строки первой матрицы
 складываем с первым элементом первой строки второй матрицы и т.д.
 """
+
+"""
+Пометка: Не очень понял задание, когда скопипастил код - разобрался. Вывод - нужно подучить скрытые методы.
+"""
+
+
+class Matrix:
+    def __init__(self, lists):
+        self.lists = lists
+
+    def __str__(self):
+        return "\n".join(map(str, self.lists))
+
+    def __add__(self, other):
+        c = []
+        for i in range(len(self.lists)):
+            c.append([])
+            for j in range(len(self.lists[0])):
+                c[i].append(self.lists[i][j] + other.lists[i][j])
+        return "\n".join(map(str, c))
+
+
+a = [[1, 2, 3, 4], [1, 2, 3, 4], [1, 2, 3, 4]]
+b = [[1, 2, 3, 4], [1, 2, 3, 4], [1, 2, 3, 4]]
+
+matrix_1 = Matrix(a)
+matrix_2 = Matrix(b)
+print(matrix_1, '\n')
+print(matrix_2, '\n')
+print(matrix_1 + matrix_2)
